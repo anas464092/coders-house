9b8abafd35882dfeafdd1c82b112c5c11745cba9
Anas Rehman <anasrehman464092@gmail.com>
2024-09-21 08:24:07 -0700

    Navbar addded

--- frontend/src/Components/Shared/Navigation/Navigation.jsx
+++ frontend/src/Components/Shared/Navigation/Navigation.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const logoStyle = {
+    color: "#fff",
+    textDecoration: "none",
+    userSelect: "none",
+  };
+
+  return (
+    <nav className={` ${styles.navabar} container`}>
+      <Link style={logoStyle} to="/">
+        <h3>👋 CodersHouse</h3>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navigation;
